73423ca99d755ad6842c3f5593d759d2218217cf
Luana <lulu-so@hotmail.com>
2023-05-30 20:10:12 -0300

    feat: adding context to the challenge

--- javascript/customer-success-balancing.js
+++ javascript/customer-success-balancing.js
@@ -4,19 +4,53 @@
  * @param {array} customers
  * @param {array} customerSuccessAway
  */
+
 function customerSuccessBalancing(
   customerSuccess,
   customers,
-  customerSuccessAway
+  customerSuccessAway 
 ) {
-  /**
-   * ===============================================
-   * =========== Write your solution here ==========
-   * ===============================================
-   */
+  
+  const css = customerSuccess.map(cs => ({ id: cs.id, score: cs.score }));
+  const csAway = customerSuccessAway || [];
+  const availableCSs = css.filter(cs => !csAway.includes(cs.id));
+
+  const csCount = availableCSs.length;
+  const csScores = availableCSs.map(cs => cs.score).sort((a, b) => b - a);
+  const customerCount = customers.length;
+  const customerScores = customers.map(customer => customer.score).sort((a, b) => b - a);
+
+  //variáveis utilizadas para controlar o index atual
+  let csIndex = 0;
+  let customerIndex = 0;
+  let customerCountByCS = Array(csCount).fill(0);
+
+  //Implementar lógica para distribuir clientes com base no score.
+  while (csIndex < csCount && customerIndex < customerCount) {
+    if (customerScores[customerIndex] <= csScores[csIndex]) {
+      customerCountByCS[csIndex]++;
+      customerIndex++;
+    } else {
+      csIndex++;
+    }
+  }
+
+  //Determinar qual css recebeu mais clientes
+  const maxCustomerCount = Math.max(...customerCountByCS);
+
+  if (customerCountByCS.filter(count => count === maxCustomerCount).length > 1) {
+    return 0; // Empate
+  }
+
+  const maxCustomerIndex = customerCountByCS.findIndex(count => count === maxCustomerCount);
+
+  
+  return availableCSs[maxCustomerIndex].id;
+
 }
 
-test("Scenario 1", () => {
+
+test("Should correctly return the id of the cs with more customers", () => {
   const css = [
     { id: 1, score: 60 },
     { id: 2, score: 20 },
@@ -55,7 +89,7 @@ function arraySeq(count, startAt){
   return Array.apply(0, Array(count)).map((it, index) => index + startAt);
 }
 
-test("Scenario 2", () => {
+test("Should return a tie when there is more than one CS with the same number of customers", () => {
   const css = mapEntities([11, 21, 31, 3, 4, 5]);
   const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
   const csAway = [];
@@ -63,7 +97,8 @@ test("Scenario 2", () => {
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
 });
 
-test("Scenario 3", () => {
+//test failing
+test("Should return the id of the cs with more customers", () => {
   const testTimeoutInMs = 100;
   const testStartTime = new Date().getTime();
 
@@ -78,7 +113,7 @@ test("Scenario 3", () => {
   }
 });
 
-test("Scenario 4", () => {
+test("Should return a draw when none of the clients are serviced", () => {
   const css = mapEntities([1, 2, 3, 4, 5, 6]);
   const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
   const csAway = [];
@@ -86,7 +121,7 @@ test("Scenario 4", () => {
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
 });
 
-test("Scenario 5", () => {
+test("Should return the id correctly when all customers are serviced by the same cs", () => {
   const css = mapEntities([100, 2, 3, 6, 4, 5]);
   const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
   const csAway = [];
@@ -94,7 +129,7 @@ test("Scenario 5", () => {
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(1);
 });
 
-test("Scenario 6", () => {
+test("Should return tie when no available cs is able to service clients", () => {
   const css = mapEntities([100, 99, 88, 3, 4, 5]);
   const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
   const csAway = [1, 3, 2];
@@ -102,7 +137,8 @@ test("Scenario 6", () => {
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(0);
 });
 
-test("Scenario 7", () => {
+//test failing
+test("Should correctly return the id of the cs that serves all customers when the other cs have higher levels", () => {
   const css = mapEntities([100, 99, 88, 3, 4, 5]);
   const customers = mapEntities([10, 10, 10, 20, 20, 30, 30, 30, 20, 60]);
   const csAway = [4, 5, 6];
@@ -110,7 +146,7 @@ test("Scenario 7", () => {
   expect(customerSuccessBalancing(css, customers, csAway)).toEqual(3);
 });
 
-test("Scenario 8", () => {
+test("Should correctly return the id of the cs that serves more customers", () => {
   const css = mapEntities([60, 40, 95, 75]);
   const customers = mapEntities([90, 70, 20, 40, 60, 10]);
   const csAway = [2, 4];
